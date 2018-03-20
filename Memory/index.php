6ab1c7a2194c65297d27e14b97b7debdeed49b98
Daegarys <midorikawakatsumiii@gmail.com>
2018-03-20 12:07:41 +0100

    View start page

--- Memory/index.php
+++ Memory/index.php
@@ -3,8 +3,19 @@
     <head>
         <meta charset="utf-8">
         <title>Memory</title>
+        <link rel="stylesheet" href="css/master.css">
     </head>
     <body>
-
+        <div class="wrapper">
+            <main>
+                <h1>Memory for kids!</h1>
+                <div class="buttons">
+                    <button type="button" name="button">speler tegen speler</button>
+                    <button type="button" name="button">aleen met tijd</button>
+                    <button class="highscore-btn" type="button" name="button">highscores</button>
+                </div>
+            </main>
+        </div>
+        <footer>Team km/s &copy; 2018</footer>
     </body>
 </html>
